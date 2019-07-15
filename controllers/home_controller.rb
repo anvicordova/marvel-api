97ee95f0caca059baf327de95347445b95443768
Angela <anvicordova@gmail.com>
2019-07-14 23:45:31 -0500

    Display information of the stories and characters for a story

--- controllers/home_controller.rb
+++ controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
   get "/" do
-    @character = MarvelCharacterService.new.find_by_name("storm")
+    character = MarvelCharacterService.new.find_by_name("storm")
+    stories = MarvelCharacterService.new.find_stories_for_character(character.id)
+    characters =  MarvelStoryService.new.find_characters_for_story(stories[1].id)
+
+    @story = {
+      character: character,
+      stories: stories,
+      characters: characters
+    }
     erb :index
   end
 end

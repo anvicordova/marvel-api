a2ef1f04593d23dda5afcb3d636dc9b68c6aa1b9
Angela <anvicordova@gmail.com>
2022-01-17 22:15:46 +0100

    Fix autoloading of files
    By using require_relative instead of require

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require "sinatra"
 require "sinatra/base"
 require "dotenv/load"
 
-Dir[File.join("controllers", "**/*_controller.rb")].each { |file| require File.expand_path(file) }
-Dir[File.join("services", "**/*.rb")].each { |file| require File.expand_path(file) }
-Dir[File.join("models", "**/*.rb")].each { |file| require File.expand_path(file) }
-Dir[File.join("decorators", "**/*.rb")].each { |file| require File.expand_path(file) }
+Dir[File.join("controllers", "**/*_controller.rb")].each { |file| require_relative File.expand_path(file) }
+Dir[File.join("services", "**/*.rb")].each { |file| require_relative File.expand_path(file) }
+Dir[File.join("models", "**/*.rb")].each { |file| require_relative File.expand_path(file) }
+Dir[File.join("decorators", "**/*.rb")].each { |file| require_relative File.expand_path(file) }
